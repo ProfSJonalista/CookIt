2e3f050c9d988d2f1bf416be9053d2cbf61f0bd2
Jan Miętki <jan.mietki@gmail.com>
2019-01-08 23:21:15 +0100

    Test

--- CookIt/CookIt/MainPage.xaml.cs
+++ CookIt/CookIt/MainPage.xaml.cs
@@ -9,29 +9,9 @@ namespace CookIt
 {
 	public partial class MainPage : ContentPage
 	{
-        int i = 0;
-
-		public MainPage()
+        public MainPage()
 		{
 			InitializeComponent();
 		}
-
-        private void Button_Clicked(object sender, EventArgs e)
-        {
-            string text = string.Empty;
-            switch (i)
-            {
-                case 0:
-                    text = i.ToString();
-                    i = 1;
-                    break;
-                case 1:
-                    text = i.ToString();
-                    i = 0;
-                    break;
-            }
-
-            MainLabel.Text = text;
-        }
     }
 }
